bb0df9ea98c41ee6f6c778978c04cba99e949075
ayyoub <ayyoub@matich.com>
2024-03-29 01:30:30 +0100

    I coding all file sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,8 @@
+-- hat prints the full description of 
+-- the table first_table from the database hbtn_0c_0 in your MySQL server.
+
+SELECT CONCAT('Table: ', table_name) AS 'Table',
+       CONCAT('Create Table: ', create_statement) AS 'Create Table'
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_0c_0'
+  AND table_name = 'first_table';
