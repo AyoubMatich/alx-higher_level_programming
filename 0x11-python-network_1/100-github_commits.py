c25934b4dc5d955136ae5039f566d161a450d012
ayyoub <matichayyoub88@gmail.com>
2024-05-10 23:41:12 +0200

    coding 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script prints the 10 most recent commits on the
+    master branch of the repository “rails” by the user "rails"""
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+    REPO, OWNER = argv[1:3]
+    url = f'https://api.github.com/repos/{OWNER}/{REPO}/commits'
+    resp = get(url)
+
+    for i in range(10):
+        try:
+            NAME = resp.json()[i].get('commit').get('author').get('name')
+            SHA = resp.json()[i].get('sha')
+            print(f'{SHA}: {NAME}')
+        except (AttributeError, IndexError):
+            break
